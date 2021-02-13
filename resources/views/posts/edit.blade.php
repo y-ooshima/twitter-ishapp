4da456393f179330e37ffde7470e739707069cc6
y-ooshima <gozira.1kawa@gmail.com>
2021-02-13 22:13:48 +0900

    fix posts layouts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,7 +1,26 @@
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+
+<h1>Editing Post</h1>
+
 <form method="POST" action="/posts/{{ $post->id }}">
-        {{ csrf_field() }}
-        <input type="hidden" name="_method" value="PUT">
-        <input type="text" name="title" value="{{ $post->title }}">
-        <input type="text" name="content" value="{{ $post->content }}">
-        <input type="submit">
-    </form> 
+    {{ csrf_field() }}
+    <input type="hidden" name="_method" value="PUT">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Title</label>
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ $post->title }}">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Content</label>
+        <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">Submit</button>
+</form>
+
+<a href="/posts/{{ $post->id }}">Show</a> | 
+<a href="/posts">Back</a>
+
+@endsection
