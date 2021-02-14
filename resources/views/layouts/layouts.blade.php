d59d96c53ac7f47fc25ce1ba9cfee327601c7bda
y-ooshima <gozira.1kawa@gmail.com>
2021-02-14 09:43:37 +0900

    fix header link and set header sticky

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -8,7 +8,7 @@
     <body>
         @component('components.header')
         @endcomponent
-        <div class="container">
+        <div class="container pt-5 pb-5">
             @yield('content')
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', 'PostController@index');
+
 Route::resource('posts', 'PostController');
 
 // if (env('APP_ENV') === 'local') {
